47494f97e7c202b564b03961fcb1fb2da483784d
insorang2 <insorang@gmail.com>
2026-06-16 17:07:52 +0900

    update

---
+++ play.js
@@ -0,0 +1,10 @@
+document.getElementById("forPuzzle").innerHTML = `
+<img
+    src="images/puzzle.png"
+    style="
+        width:90%;
+        max-width:900px;
+        margin-top:30px;
+    "
+>
+`;
