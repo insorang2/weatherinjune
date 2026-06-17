18a38f0800bf70ad8bf23b5267a9404f5a38aa4b
insorang2 <insorang@gmail.com>
2026-06-17 14:45:10 +0900

    Update play.js

--- play.js
+++ play.js
@@ -5,7 +5,7 @@ const image = new Image();
 
 image.src = "images/puzzle.png";
 
-image.onload = function() {
+image.onload = function(){
 
     ctx.drawImage(
         image,
@@ -15,4 +15,53 @@ image.onload = function() {
         canvas.height
     );
 
+    createPieces();
 };
+
+function createPieces(){
+
+    const piecesDiv =
+        document.getElementById("pieces");
+
+    piecesDiv.innerHTML = "";
+
+    const rows = 10;
+    const cols = 10;
+
+    const pieceWidth =
+        image.width / cols;
+
+    const pieceHeight =
+        image.height / rows;
+
+    for(let y=0;y<rows;y++){
+
+        for(let x=0;x<cols;x++){
+
+            const piece =
+                document.createElement("canvas");
+
+            piece.width = pieceWidth;
+            piece.height = pieceHeight;
+
+            piece.className = "piece";
+
+            const pctx =
+                piece.getContext("2d");
+
+            pctx.drawImage(
+                image,
+                x * pieceWidth,
+                y * pieceHeight,
+                pieceWidth,
+                pieceHeight,
+                0,
+                0,
+                pieceWidth,
+                pieceHeight
+            );
+
+            piecesDiv.appendChild(piece);
+        }
+    }
+}
